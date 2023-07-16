2aadeacf36c17fd1d1256af5f6cfa362197c2a8e
Rubendvb <rubenvasquez556@gmail.com>
2023-07-15 23:00:18 -0300

    refatorando interfaces

--- src/@types/comments.d.ts
+++ src/@types/comments.d.ts
@@ -1,22 +1,32 @@
+export interface IUserImage {
+  png: string;
+  webp: string;
+}
+
+export interface IUser {
+  image: IUserImage;
+  username: string;
+}
+
 export interface IReply {
   id: number;
   content: string;
   createdAt: string;
   score: number;
   replyingTo: string;
-  user: {
-    image: {
-      png: string;
-      webp: string;
-    };
-    username: string;
-  };
+  user: IUser;
+}
+
+export interface IComment {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  user: IUser;
+  replies: IReply[];
 }
 
 export interface ICurrentUser {
-  image?: {
-    png: string;
-    webp: string;
-  };
-  username?: string;
+  image: IUserImage;
+  username: string;
 }
