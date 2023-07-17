6150cc4e793ceadc4361a728744e1489888c4c25
Rubendvb <rubenvasquez556@gmail.com>
2023-07-16 23:33:25 -0300

    alinhando elemento you

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -29,42 +29,44 @@ export default function Card({ comment, setShowForm }: ICard) {
                 src={comment.user.image.png}
                 alt={comment.user.username}
               />
-              <span className="card__header__container__name">
-                {comment.user.username}
-              </span>
 
-              {userName === comment.user.username && (
-                <span className="card__header__container__you">you</span>
-              )}
+              <div>
+                <span className="card__header__container__name">
+                  {comment.user.username}
+                </span>
+                {userName === comment.user.username && (
+                  <span className="card__header__container__you">you</span>
+                )}
+              </div>
 
               <span className="card__header__container__date">
                 {comment.createdAt}
               </span>
             </div>
+
+            <div className="card__footer">
+              {userName === comment.user.username ? (
+                <div className="card__footer__buttons">
+                  <div className="card__footer__buttons__delete containerButtons">
+                    <img src={Delete} alt="" />
+                    <span>Delete</span>
+                  </div>
+
+                  <div className="card__footer__buttons__edit containerButtons">
+                    <img src={Edit} alt="" />
+                    <span>Edit</span>
+                  </div>
+                </div>
+              ) : (
+                <div className="card__header__reply">
+                  <ButtonReply setShowForm={setShowForm} />
+                </div>
+              )}
+            </div>
           </div>
           <div className="card__body">
             <p className="card__body__text">{comment.content}</p>
           </div>
-        </div>
-
-        <div className="card__footer">
-          {userName === comment.user.username ? (
-            <div className="card__footer__buttons">
-              <div className="card__footer__buttons__delete containerButtons">
-                <img src={Delete} alt="" />
-                <span>Delete</span>
-              </div>
-
-              <div className="card__footer__buttons__edit containerButtons">
-                <img src={Edit} alt="" />
-                <span>Edit</span>
-              </div>
-            </div>
-          ) : (
-            <div className="card__header__reply">
-              <ButtonReply setShowForm={setShowForm} />
-            </div>
-          )}
         </div>
       </article>
     </>
